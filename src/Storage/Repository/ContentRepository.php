f8dded3438803e3b99f4a22ea38e81af2de89352
Ross Riley <riley.ross@gmail.com>
2015-05-29 08:21:14 +0100

    setup separate repository folder to store custom repository classes

---
+++ src/Storage/Repository/ContentRepository.php
@@ -0,0 +1,36 @@
+<?php
+namespace Bolt\Storage\Repository;
+
+use Bolt\Entity\Content;
+
+/**
+ * A Repository class that handles dynamically created content tables.
+ */
+class ContentRepository extends Repository
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function createQueryBuilder($alias = null, $indexBy = null)
+    {
+        if (null === $alias) {
+            $alias = $this->getAlias();
+        }
+        return $this->em->createQueryBuilder()
+            ->select($alias.".*")
+            ->from($this->getTableName(), $alias);
+    }
+
+    /**
+     * Creates a new Content entity and passes the supplied data to the constructor.
+     *
+     * @param array $params
+     *
+     * @return Content
+     */
+    public function create($params = null)
+    {
+        return new Content($params);
+    }
+}
---
+++ src/Storage/Repository/CronRepository.php
@@ -0,0 +1,38 @@
+<?php
+namespace Bolt\Storage\Repository;
+
+use Bolt\Storage\Repository;
+
+/**
+ * A Repository class that handles storage operations for the Cron table.
+ */
+class CronRepository extends Repository
+{
+
+ 
+    public function getNextRunTimes($interimName)
+    {
+        $query = $this->queryNextRunTimes($interimName);
+        return $query->execute()->fetchAll();
+    }
+    
+    public function queryNextRunTimes($interimName)
+    {
+        $oldname = strtolower(str_replace('cron.', '', $interimName));
+        $qb = $this->createQueryBuilder();
+        $qb->select('lastrun, interim')
+            ->where('(interim = :interim OR interim = :oldname)')
+            ->orderBy('lastrun', 'DESC')
+            ->setParameter('interim', $interimName)
+            ->setParameter('oldname', $oldname);
+        return $qb;
+    }
+    
+    public function createQueryBuilder($alias = NULL, $indexBy = NULL)
+    {
+        return $this->em->createQueryBuilder()
+            ->from($this->getTableName());
+    }
+
+
+}
