aaa07eb09a53b50a2b7a8f8dc9bacf67e473ffe8
Ross Riley <riley.ross@gmail.com>
2015-05-30 11:46:13 +0100

    add new set of cron test assertions

--- tests/phpunit/unit/Cron/CronTest.php
+++ tests/phpunit/unit/Cron/CronTest.php
@@ -18,12 +18,16 @@ class CronTest extends BoltUnitTest
         $app = $this->getApp();        
         $output = new BufferedOutput();
         
-        $cron = new Cron($app, $output);
-        $cron->execute();
+        $cron = new Cron($app, $output);        
+        $result = explode("\n", $output->fetch());
         
-        $result = $output->fetch();
+        $this->assertEquals("Running Cron Hourly Jobs", $result[0]);
+        $this->assertEquals("Running Cron Daily Jobs", $result[1]);
+        $this->assertEquals("Running Cron Weekly Jobs", $result[2]);
+        $this->assertEquals("Clearing cache", trim($result[3]));
+        $this->assertEquals("Trimming logs", trim($result[4]));
+        $this->assertEquals("Running Cron Monthly Jobs", $result[5]);
+        $this->assertEquals("Running Cron Yearly Jobs", $result[6]);
         
-        var_dump($output);
-
     }
 }
